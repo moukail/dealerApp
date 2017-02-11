85e3ac8b579a0a7e04f5ac8498c64dae35206589
Ismail Moukafih <moukafih@live.nl>
2017-02-11 11:55:58 +0100

    update

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,5 +5,4 @@ echo "-------------------------------------------------------------------"
 
 composer update
 composer development-enable
-composer development-enable
 php public/index.php orm:schema-tool:update --dump-sql --force
